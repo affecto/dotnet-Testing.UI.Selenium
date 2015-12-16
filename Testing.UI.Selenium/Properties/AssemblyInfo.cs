d677a42c1b2f96157b517ed398bcddc4716c0244
Ville Henriksson <teettari@hotmail.com>
2015-12-16 13:50:31 +0200

    Release version of NuGet package.

--- Testing.UI.Selenium/Properties/AssemblyInfo.cs
+++ Testing.UI.Selenium/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@
 [assembly: AssemblyFileVersion("1.8.0.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("1.8.0-prerelease01")]
+[assembly: AssemblyInformationalVersion("1.8.0")]
